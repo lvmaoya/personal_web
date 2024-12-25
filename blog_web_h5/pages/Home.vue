8bb6b5be4203a7dc7d3016265be413a426cb9b58
sunjianxiang <1504734652@qq.com>
2024-12-25 15:25:35 +0800

    feat：添加google search console认证

--- blog_web_h5/pages/Home.vue
+++ blog_web_h5/pages/Home.vue
@@ -8,7 +8,11 @@ import { type ThingType } from "@/composables/index";
 const thingList = ref<Array<ThingType>>();
 const emits = defineEmits(['contentChange'])
 thingList.value = (await getIntersetThingList()).data;
-
+useHead({
+  meta: [
+    { name: "google-site-verification", content: "rdicKFHUWZtMhhwgAXfrjwCJSXuzRzH6UcmV1gUgj_o" },
+  ],
+});
 onBeforeMount(() => {
   emits('contentChange')
 })
